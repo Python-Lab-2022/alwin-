75a03c1930369058718f262078fc42abad32aa30
alwiavara11 <118240944+alwiavara11@users.noreply.github.com>
2023-01-19 12:06:40 +0530

    Delete color.py

--- color.py
+++
@@ -1,7 +0,0 @@
-list=[]
-n=int(input("enter the colours:"))
-for i in range(0,n):
-    list.append(str(input()))
-print(list)
-print("the first colour is",list[0])
-print("the last colour is",list[-1])
